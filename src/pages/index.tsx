3f4c373a38f8a8b4d2c8d16346485daeceaea04a
KevinYu0515 <yellow0515car@gmail.com>
2023-12-13 01:49:05 +0800

    測試機器人發送

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,7 +27,7 @@ const Home: NextPage = () => {
             ref={ref}
           />
           <div className="btn-group w-full">
-            <a href={`/messages?text=${ref.current?.value || ""}`} className="btn w-1/3">文字訊息</a>
+            <a href={`/build?text=${ref.current?.value || ""}`} className="btn w-1/3">文字訊息</a>
             <a href={`/create/hongbao`} className="btn-error btn w-1/3">紅包</a>
             <a href={`/create/linepay`} className="btn-success btn w-1/3">LinePay</a>
           </div>
