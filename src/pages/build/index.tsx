263eba466ba53b001536f8e6b956d28295e0e449
KevinYu0515 <yellow0515car@gmail.com>
2023-12-13 02:22:05 +0800

    放棄機器人發送訊息

--- src/pages/build/index.tsx
+++ src/pages/build/index.tsx
@@ -1,17 +1,12 @@
 import { type NextPage } from "next";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import * as line from '@line/bot-sdk';
-const { MessagingApiClient, } = line.messagingApi;
 
 type PermissionState = {
     canWriteMessage: boolean;
     message: string;
     background: string;
 };
-
-const channelAccessToken = 'JRW5NABtAZzyxrg9nWzAI9LFI+9hnskwaiwE6bKAsKkO07KHlHBXdHZc8HciBqnF2tcOhm+mre0OsHE2n9uwS0qv3PrHV4PHJIe9Yx7Ik3hve4rigeo+kDiP7BeOj6JikGhTXo5fFDDYB24iTyvVowdB04t89/1O/w1cDnyilFU=';
-const userId = "Uacd83125262a3b77850e06ec151c5ec3";
 
 const LineMSBuild: NextPage = () => {
     const router = useRouter();
@@ -23,40 +18,11 @@ const LineMSBuild: NextPage = () => {
     });
 
     useEffect(() => {
-        const handleSendMessage = async (message: string) => {
-            console.log(message)
-            const client = new MessagingApiClient({
-                channelAccessToken
-            });
-
-            try {
-              const textMessage: line.TextMessage = {
-                type: 'text',
-                text: message,
-              };
-        
-              const messageRequest = {
-                to: userId, 
-                messages: [textMessage],
-              };
-              
-              await client.pushMessage(messageRequest);        
-            } catch (error) {
-              console.error('Error sending message:', error);
-            }
-        };
-    
         setState({
           canWriteMessage: true,
-          message: "你的整人連結做好ㄌ，請回去 Line 看看",
+          message: `這是你的連結：\n ${router.query.text as string}`,
           background: "from-[#f78ca0] to-[#fe9a8b]",
         });
-    
-        if (router.query.text) {
-          const urlParams = new URLSearchParams(window.location.search);
-          const text = urlParams.get('text')
-          handleSendMessage(text as string).catch(console.log);
-        }
       }, [router.query.text]);
     return (
         <main
